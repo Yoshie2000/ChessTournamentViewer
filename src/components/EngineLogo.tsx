8d09eb0ac888a81c962c08164c539111578ef4ad
truekendor <sadqwerty000@gmail.com>
2026-03-01 14:20:03 +0300

    alter engine logos depending on TCEC vs CCC

--- src/components/EngineLogo.tsx
+++ src/components/EngineLogo.tsx
@@ -4,23 +4,34 @@ import "./EngineLogo.css";
 type EngineLogoProps = { engine?: CCCEngine; size?: number };
 
 function getImageUrl(engine?: CCCEngine) {
-  let imgSrc = "";
   if (engine?.imageUrl.includes("https")) {
-    imgSrc = engine.imageUrl;
-  } else if (window.location.search.includes("tcec")) {
-    imgSrc = "https://ctv.yoshie2000.de/tcec/image/tcec2.jpg";
-  } else if (engine?.imageUrl) {
-    imgSrc =
-      "https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/" +
-      engine.imageUrl +
-      ".png";
+    return engine.imageUrl;
   }
 
-  return imgSrc;
+  if (window.location.search.includes("tcec")) {
+    if (engine?.imageUrl) {
+      const engineName = `${engine.imageUrl[0].toUpperCase()}${engine.imageUrl.slice(1).toLowerCase()}`;
+      return `https://ctv.yoshie2000.de/tcec/image/engine/${engineName}.png`;
+    }
+
+    return "https://ctv.yoshie2000.de/tcec/image/tcec2.jpg";
+  }
+
+  if (engine?.imageUrl) {
+    return `https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/${engine.imageUrl}.png`;
+  }
+
+  return "https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/heimdall.png";
 }
 
 export function EngineLogo({ engine, size = 36 }: EngineLogoProps) {
   const src = getImageUrl(engine);
+
+  // const src1 = engine?.imageUrl.includes("https")
+  //   ? engine.imageUrl
+  //   : "https://images.chesscomfiles.com/chess-themes/computer_chess_championship/avatars/" +
+  //     engine.imageUrl +
+  //     ".png";
 
   return (
     <img
