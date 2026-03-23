7eed9cb70fb489edd56f9061bcefd541146f5c9f
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-03-23 21:16:16 +0100

    WIP

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,9 @@ import { StandingsWindow } from "./components/StandingsWindow/StandingsWindow";
 import { BoardWindow } from "./components/BoardWindow/BoardWindow";
 import { ScheduleWindow } from "./components/ScheduleWindow/ScheduleWindow";
 import { Popup } from "./components/Popup/Popup";
+import { ResponsiveGridLayout } from "react-grid-layout";
+import { useWindowSize } from "./hooks/useWindowSize";
+import { useEffect } from "react";
 
 Chart.register(
   CategoryScale,
@@ -29,21 +32,75 @@ Chart.register(
 );
 
 function App() {
-  return (
-    <div className="app">
-      <Popup />
+  const COLUMNS = 48;
 
+  const { width, height } = useWindowSize();
+  const cellSize = Math.floor(width / COLUMNS);
+  const rows = Math.floor((height - 56) / cellSize);
+
+  const boardWidth = 20 * cellSize;
+  const boardHeight = 16 * cellSize;
+
+  useEffect(() => {
+    document.documentElement.style.setProperty(
+      "--boardWidth",
+      `${boardWidth}px`
+    );
+    document.documentElement.style.setProperty(
+      "--boardHeight",
+      `${boardHeight}px`
+    );
+  }, [boardWidth, boardHeight]);
+
+  console.log(cellSize, width, height, rows)
+
+  return (
+    <div className="app-container">
+      <Popup />
       <EventListWindow />
 
-      <EngineWindow />
+      <ResponsiveGridLayout
+        className="app-grid"
+        width={width - 8}
+        cols={{
+          lg: COLUMNS,
+          md: COLUMNS,
+          sm: COLUMNS,
+          xs: COLUMNS,
+          xxs: COLUMNS,
+        }}
+        rowHeight={cellSize}
+        autoSize={true}
+        containerPadding={[0, 0]}
+        margin={[0, 0]}
+        maxRows={rows}
+        dragConfig={{ enabled: true, handle: ".react-grid-drag-handle" }}
+      >
+        <div key={5} data-grid={{ w: 12, h: rows, x: 36, y: 0 }}>
+          <EngineWindow />
+          <div className="react-grid-drag-handle" />
+        </div>
 
-      <BoardWindow />
+        <div key={1} data-grid={{ w: 20, h: 16, x: 16, y: 0 }}>
+          <BoardWindow />
+          <div className="react-grid-drag-handle" />
+        </div>
 
-      <StandingsWindow />
+        <div key={3} data-grid={{ w: 16, h: 10, x: 0, y: 24 }}>
+          <StandingsWindow />
+          <div className="react-grid-drag-handle" />
+        </div>
 
-      <GraphWindow />
+        <div key={4} data-grid={{ w: 20, h: 10, x: 16, y: 16 }}>
+          <GraphWindow />
+          <div className="react-grid-drag-handle" />
+        </div>
 
-      <ScheduleWindow />
+        <div key={2} data-grid={{ w: 16, h: 16, x: 0, y: 0 }}>
+          <ScheduleWindow />
+          <div className="react-grid-drag-handle" />
+        </div>
+      </ResponsiveGridLayout>
     </div>
   );
 }
