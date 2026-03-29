a9c759544db53d202677b8594a2963d6f4ed8626
Yoshie2000 <patrick.leonhardt@gmx.net>
2026-03-29 21:45:49 +0200

    try again

--- src/components/ScheduleWindow/TwitchChat.tsx
+++ src/components/ScheduleWindow/TwitchChat.tsx
@@ -3,12 +3,13 @@ import { useEventStore } from "../../context/EventContext";
 export function TwitchChat() {
   const activeProvider = useEventStore((state) => state.activeProvider);
 
+  const domain = location.hostname;
   const twitchChannel =
     activeProvider === "ccc" ? "computerchess" : "TCEC_Chess_TV";
 
   return (
     <iframe
-      src={`https://www.twitch.tv/embed/${twitchChannel}/chat?parent=staging.ctv.yoshie2000.de&parent=ctv.yoshie2000.de&darkpopout`}
+      src={`https://www.twitch.tv/embed/${twitchChannel}/chat?parent=${domain}&darkpopout`}
       style={{ width: "100%", height: "100%" }}
       loading="lazy"
     />
