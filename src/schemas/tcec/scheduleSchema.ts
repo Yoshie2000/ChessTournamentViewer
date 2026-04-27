4fce74fb654b07e371d04781c0e2257e197bdbb5
truekendor <sadqwerty000@gmail.com>
2026-04-27 18:04:54 +0300

    remove comments and TODO's

--- src/schemas/tcec/scheduleSchema.ts
+++ src/schemas/tcec/scheduleSchema.ts
@@ -1,19 +1,9 @@
 import z from "zod";
 
 const matchResult = z.enum(["1/2-1/2", "1-0", "0-1", "*"]);
-// TODO:
-// const termination = z.enum([
-//   "3-Fold repetition",
-//   "TCEC draw rule",
-//   "in progress",
-//   "White mates",
-//   "Black mates",
-//   "Fifty moves rule",
-// ]);
-const roomType = z.enum(["roomall"]); // can be other value??
+const roomType = z.enum(["roomall"]);
 export const htmlReadSchema = z.object({ room: roomType, data: z.string() });
 
-/// schema 1
 const futureGameSchema = z.object({
   Black: z.string(),
   White: z.string(),
@@ -21,12 +11,7 @@ const futureGameSchema = z.object({
 });
 
 const currentGameSchema = z
-  .object({
-    Result: matchResult,
-    // Termination: termination,
-    Termination: z.string(),
-    Start: z.string(),
-  })
+  .object({ Result: matchResult, Termination: z.string(), Start: z.string() })
   .and(futureGameSchema);
 
 const pastGameSchema = z
