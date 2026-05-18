2545eaba9f70812304557558848c2f27426cab10
truekendor <sadqwerty000@gmail.com>
2026-05-18 15:15:20 +0300

    update schema

--- src/schemas/tcec/liveInfoSchema.ts
+++ src/schemas/tcec/liveInfoSchema.ts
@@ -2,7 +2,7 @@ import z from "zod";
 
 export const liveInfoSchema = z.object({
   depth: z.string(),
-  eval: z.number(),
+  eval: z.union([z.number(), z.string()]),
   nodes: z.number(),
   pv: z.string(),
   speed: z.string(),
